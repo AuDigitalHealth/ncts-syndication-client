efa9c026de84c686f3f70f5198c22dc714840664
patrick <patrick@192.168.19.109>
2018-05-01 16:41:39 +1000

    Added testng tests for client utilising mockserver

--- ncts-syndication-client-library/src/test/java/com/github/dionmcm/ncts/syndication/client/clientTest.java
+++ ncts-syndication-client-library/src/test/java/com/github/dionmcm/ncts/syndication/client/clientTest.java
@@ -25,10 +25,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.commons.io.FileUtils;
@@ -79,32 +82,61 @@ public class clientTest {
     public void downloadsAllFilesInCategory() throws URISyntaxException, IOException, NoSuchAlgorithmException, JDOMException, HashValidationFailureException {
     	testClient = new SyndicationClient(feedURL,tokenURL, outDir, clientID, secret);
     	Map<String, List<DownloadResult>> result = testClient.download(false, "SCT_RF2_BLUE");
+    	
+    	List<DownloadResult> blueResults = result.get("SCT_RF2_BLUE");
+    	
+    	//return list of all downloaded blue files
+    	List<String> downloadedFiles = blueResults.stream()
+    									.map(file -> file.getFile().getName())
+    									.collect(Collectors.toList());
+    	
+    	//assert that all blue results in list
     	assertTrue(
-    		Files.list(outDir.toPath())
-    			.anyMatch(fileName -> fileName.getFileName().toString().equals("blue1.r2")) &&
-    		Files.list(outDir.toPath())
-    			.anyMatch(fileName -> fileName.getFileName().toString().equals("blue2.r2"))
+    		downloadedFiles.contains("blue1.r2") &&
+    		downloadedFiles.contains("blue2.r2")
     	);
+
+    	/*
+    	//assert files not missing from local directory
+    	assertFalse(
+    		Files.list(outDir.toPath())
+    			.map(file -> file.getFileName().toString())
+    			.noneMatch(fileName -> fileName.equals("blue1.r2") ||
+    									fileName.equals("blue2.r2"))
+    	);*/
     }
     
+    
+
     @Test(priority = 2, groups = "downloading", enabled = true )
     public void downloadsLatestInCategory() throws IOException, URISyntaxException, NoSuchAlgorithmException, JDOMException, HashValidationFailureException {
     	testClient = new SyndicationClient(feedURL,tokenURL, outDir, clientID, secret);
     	DownloadResult result = testClient.downloadLatest("SCT_RF2_PURPLE");
-    	assertTrue(
-    		Files.list(outDir.toPath())
-    			.anyMatch(fileName -> fileName.getFileName().toString().equals("purple2.r2"))
-    	);
-    	assertFalse(
-        		Files.list(outDir.toPath())
-        			.anyMatch(fileName -> fileName.getFileName().toString().equals("purple1.r2"))
-        	);
+    	assertTrue(result.getFile().getName().equals("purple2.r2"));
     }
     
     @Test(priority = 3, groups = "downloading", enabled = true, expectedExceptions = HashValidationFailureException.class)
     public void hashMismatchInSyndicationThrowsException() throws IOException, URISyntaxException, NoSuchAlgorithmException, JDOMException, HashValidationFailureException{
     	testClient = new SyndicationClient(feedURL,tokenURL, outDir, clientID, secret);
     	DownloadResult result = testClient.downloadLatest("SCT_RF2_GREEN");
+    }
+    
+    @Test(priority = 4, groups = "downloading", enabled = true )
+    public void downloadsFilesFromMultipleCategories() throws IOException, URISyntaxException, NoSuchAlgorithmException, JDOMException, HashValidationFailureException {
+    	testClient = new SyndicationClient(feedURL,tokenURL, outDir, clientID, secret);
+    	Map<String, List<DownloadResult>> result = testClient.download(false, "SCT_RF2_PURPLE", "SCT_RF2_RED", "SCT_RF2_BLUE");
+    	
+    	List<String> downloadedFiles = result.values().stream().flatMap(category -> category.stream())
+    			.map(file -> file.getFile().getName())
+    			.collect(Collectors.toList());
+    	    	
+    	assertTrue(
+    			downloadedFiles.contains("purple1.r2") &&
+    			downloadedFiles.contains("purple2.r2") &&
+    			downloadedFiles.contains("red1.r2") &&
+    			downloadedFiles.contains("blue1.r2") &&
+    			downloadedFiles.contains("blue2.r2") 
+        );
     }
 
     
@@ -122,7 +154,10 @@ public class clientTest {
     		serverFileBins.put(aFile, Files.readAllBytes(filePath));
     	}
     	
-    	//Set up handlers for individual files
+    	File syndFilePath = new File(serverDir + "syndication.xml");
+        byte[] syndFile = Files.readAllBytes(syndFilePath.toPath());
+    	
+    	//Set up handling for individual files
     	for(String aFile : serverFileList) {
         	mockServerClient.when(HttpRequest.request().withPath("/" + aFile))
 			.respond(
@@ -132,8 +167,6 @@ public class clientTest {
     	}
     
     	//Handle request for syndication file
-    	File syndFilePath = new File("src/test/java/com/github/dionmcm/ncts/syndication/client/Test_Server/syndication.xml");
-        byte[] syndFile = Files.readAllBytes(syndFilePath.toPath());
     	mockServerClient.when(HttpRequest.request().withPath("/syndication.xml"))
     					.respond(
     							HttpResponse.response()
@@ -157,6 +190,7 @@ public class clientTest {
     public void tearDownMockServer() {
         mockServer.stop();
     }
+    
  
     
     public static void main(String[] theArgs) throws Exception {
